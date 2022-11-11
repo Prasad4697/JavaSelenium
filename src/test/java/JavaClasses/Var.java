7189bac417afa9729ea4dc6634de25f3afd4a780
Prasad4697 <prasad4697@icloud.com>
2022-11-11 19:06:11 +0530

    Added All The Files

---
+++ src/test/java/JavaClasses/Var.java
@@ -0,0 +1,19 @@
+package JavaClasses;
+
+public class Var {
+    // Global Variable
+    String city ="Bangalore";
+    public static void main(String[] args) {
+        //non Static
+        Var var = new Var();
+        var.city ="Pune";
+        System.out.println(var.city);
+        System.out.println(state);
+
+
+    }
+    public static void registration(){
+
+
+    }
+}
